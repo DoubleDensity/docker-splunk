73a42bd7b52884ea39c70b4a252da1ed8fd9a941
Buttetsu <doubledense@gmail.com>
2016-01-11 23:03:14 -0500

    attempting to add support for custom index on NFS

--- splunk/entrypoint.sh
+++ splunk/entrypoint.sh
@@ -2,6 +2,34 @@
 
 set -e
 
+# If NFS parameters are passed in then try to mount the target
+if [ -z "$NFS_EXPORT" ] && [ -z "$NFS_MOUNT" ]; then
+    if [ -e ${$NFS_MOUNT} ]; then
+        mount -t nfs $NFS_EXPORT $NFS_MOUNT
+    else
+        mkdir $NFS_MOUNT
+        mount -t nfs $NFS_EXPORT $NFS_MOUNT
+    fi
+    
+    # Create bucket dirs if they don't already exist
+    if [ ! -e ${NFS_MOUNT}/hotwarm ]; then
+        mkdir ${NFS_MOUNT}/hotwarm
+    fi
+    if [ ! -e ${NFS_MOUNT}/cold ]; then
+        mkdir ${NFS_MOUNT}/cold
+    fi
+    
+    # Populate defaults for NFS storage in local index.conf
+    echo "[nfsindex]" > ${SPLUNK_HOME}/etc/system/local/indexes.conf
+    echo "homePath=${NFS_MOUNT}/hotwarm" >> ${SPLUNK_HOME}/etc/system/local/indexes.conf
+    echo "coldPath=${NFS_MOUNT}/cold" >> ${SPLUNK_HOME}/etc/system/local/indexes.conf
+    
+    # Set our default input to use this index
+    echo "index = [nfsindex]" >> ${SPLUNK_HOME}/etc/system/local/inputs.conf
+fi
+
+
+
 if [ "$1" = 'splunk' ]; then
   shift
   sudo -HEu ${SPLUNK_USER} ${SPLUNK_HOME}/bin/splunk "$@"
