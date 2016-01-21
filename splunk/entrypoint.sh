c26221a73405788f54b9a8088d5be97b4252a5c6
Buttetsu Batou <doubledense@gmail.com>
2016-01-21 15:10:46 -0500

    adding step to create /opt/splunk/etc/myinstall/

--- splunk/entrypoint.sh
+++ splunk/entrypoint.sh
@@ -54,6 +54,7 @@ if [ -n "${FSCK+set}" ]; then
 	echo "Offline fsck requested!"
 	echo "Copying default Splunk configs from /tmp to /opt/splunk/etc"
 	cp -v /tmp/splunk-launch.conf /opt/splunk/etc
+	mkdir -p /opt/splunk/etc/myinstall/
 	cp -v /tmp/splunkd.xml.cfg-default /opt/splunk/etc/myinstall/
 	${SPLUNK_HOME}/bin/splunk fsck repair --all-buckets-all-indexes
 	exit 0;
